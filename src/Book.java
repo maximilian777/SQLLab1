7f5b3ef87d70c15cdee9860da821ebedca1d63b1
maximilian777 <maximilian.krentzel@gmail.com>
2024-12-08 19:13:39 +0100

    Small changes

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,17 @@
 
 public class Book {
     private final String title;
-    private final String publishingDate;
     private final Author author;
     private final String genre;
     private int pages;
+    private final int ISBN;
 
-    Book(String title, String publishingDate, Author author, String genre, int pages) {
+    Book(String title, Author author, String genre, int pages, int ISBN) {
         this.title = title;
-        this.publishingDate = publishingDate;
         this.author = author;
         this.genre = genre;
         this.pages = pages;
+        this.ISBN = ISBN;
     }
 }
 
